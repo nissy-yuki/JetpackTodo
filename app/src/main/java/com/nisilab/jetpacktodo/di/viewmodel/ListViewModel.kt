50e264e565b628a6c584bcdacb00b5d697bd6051
nissy-yuki <nisi20010510@gmail.com>
2022-04-25 18:15:53 +0900

    add list viewmodel variable

--- app/src/main/java/com/nisilab/jetpacktodo/di/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/nisilab/jetpacktodo/di/viewmodel/ListViewModel.kt
@@ -6,9 +6,18 @@ import androidx.lifecycle.ViewModel
 import com.nisilab.jetpacktodo.di.repository.DataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(private val repository: DataRepository) : ViewModel() {
-    private val todoItems: MutableState<TodoList> = mutableStateOf(TodoList(repository.loadItems()))
+    private val _todoItems = MutableStateFlow(TodoList(repository.loadItems()))
+    private val _outAllItems = MutableStateFlow(_todoItems.value.toOutItems())
+    private val _outItems = MutableStateFlow(_outAllItems.value)
+
+    val todoItems: StateFlow<TodoList> = _todoItems
+    val outAllItems: StateFlow<List<OutItem>> = _outAllItems
+    val outItems: StateFlow<List<OutItem>> = _outItems
+
+
 }
